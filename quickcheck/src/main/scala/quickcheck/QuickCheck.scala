cac0d3fc7c84afa46deceb316d375f571afc11f7
DRaco <draco@epf.pl>
2014-07-22 12:37:07 +0200

    first property and Heap Generator mark I added

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -14,7 +14,27 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
-  lazy val genHeap: Gen[H] = ???
+  property("gen1") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h)) == m
+  }
+
+  property("min of two el. heap") = forAll { (a: Int, b: Int) =>
+    val h = insert(b, insert(a, empty))
+    findMin(h) == (if (a <= b) a else b)
+  }
+
+  def emptyHeaps = value(empty)
+
+  def nonEmptyHeaps = for {
+    i <- arbitrary[Int]
+    heaps <- genHeap
+  } yield meld(insert(i, empty), heaps)
+
+  lazy val genHeap: Gen[H] = for {
+    isEmpty <- Arbitrary(oneOf(true, false)).arbitrary
+    h <- (if (isEmpty) emptyHeaps else nonEmptyHeaps)
+  } yield h
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
